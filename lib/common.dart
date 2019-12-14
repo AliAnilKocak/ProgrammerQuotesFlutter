783370cf28c2b9631df483eca5759927cc5c294a
Ali Anıl Koçak <alianilkocak@gmail.com>
2019-12-15 00:51:03 +0300

    Design completed

---
+++ lib/common.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+ List<Color> colors = [
+   Color(0xFFAF56F9),
+   Color(0xFF5659F9),
+   Color(0xFF56D4F9),
+   Color(0xFFF65187),
+   Color(0xFFF5CC46),
+ ];
--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:programmerquotes/common.dart' as prefix0;
 import 'package:programmerquotes/models/QuetoModel.dart';
 
 import '../data.dart';
@@ -18,9 +19,19 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           children: <Widget>[
             Container(
+              margin: EdgeInsets.only(bottom: 12),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height / 12,
               decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFFF7576B),
+                      blurRadius: 20.0,
+                      // has the effect of softening the shadow
+                      spreadRadius:
+                          1.0, // has the effect of extending the shadow
+                    )
+                  ],
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(64),
                       bottomRight: Radius.circular(64)),
@@ -41,7 +52,10 @@ class _HomePageState extends State<HomePage> {
               child: ListView(
                   shrinkWrap: true,
                   children: data.map((word) {
-                    return buildItem(word);
+                    return Padding(
+                      padding: const EdgeInsets.only(left:8.0,right: 8.0),
+                      child: buildItem(word),
+                    );
                   }).toList()),
             )
           ],
@@ -50,7 +64,52 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget buildItem(QuetoModel word) {
-    return Text(word.author);
+  Widget buildItem(QuetoModel data) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color:prefix0.colors[data.hashCode%5] ,
+            blurRadius: 5.0, // has the effect of softening the shadow
+            spreadRadius: 0.5, // has the effect of extending the shadow
+          )
+        ],
+      ),
+      margin: EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: prefix0.colors[data.hashCode%5],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(12),
+                  topLeft: Radius.circular(12),
+                )),
+            height: 136,
+            width: 16,
+          ),
+          Flexible(
+            child: Container(
+              padding: EdgeInsets.only(left: 12, right: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("\""+data.en+"\""),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text("- " + data.author))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
